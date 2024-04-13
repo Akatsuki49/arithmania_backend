586e9b0482ee5655efbbd2d2b28297ab3a159ed4
Shubh Kanodia <pes1202100702@pesu.pes.edu>
2024-04-13 23:24:37 +0530

    question_added_in_routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify, redirect
-from model_load import classify_question
+from flask import Flask, request, jsonify, redirect, url_for
+#from model_load import classify_question
 
 app = Flask(__name__)
 
@@ -7,31 +7,38 @@ app = Flask(__name__)
 def query():
     question = request.form['question']
     # Use the middleware to classify the question
-    classification = classify_question(question)
+    classification = "personal budgeting" #classify_question(question)
     # Redirect to the appropriate route based on classification
     if classification == "investment":
-        return redirect('/investments')
+        return redirect(url_for('investments', question=question))
     elif classification == "personal budgeting":
-        return redirect('/personal_budgeting')
+        return redirect(url_for('personal_budgeting', question=question))
     elif classification == "financial education":
-        return redirect('/financial_education')
+        return redirect(url_for('financial_education', question=question))
     else:
         return jsonify({'error': 'Invalid classification'})
 
 @app.route('/investments')
 def investments():
+    # Retrieve question from URL query parameters
+    question = request.args.get('question', '')
     # Implement your logic for investments route here
-    return jsonify({'message': 'Investments route'})
+
+    return jsonify({'question': question, 'message': 'Investments route'})
 
 @app.route('/personal_budgeting')
 def personal_budgeting():
+    # Retrieve question from URL query parameters
+    question = request.args.get('question', '')
     # Implement your logic for personal budgeting route here
-    return jsonify({'message': 'Personal Budgeting route'})
+    return jsonify({'question': question, 'message': 'Personal Budgeting route'})
 
 @app.route('/financial_education')
 def financial_education():
+    # Retrieve question from URL query parameters
+    question = request.args.get('question', '')
     # Implement your logic for financial education route here
-    return jsonify({'message': 'Financial Education route'})
+    return jsonify({'question': question, 'message': 'Financial Education route'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
